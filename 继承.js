5819ff709be4320a7d1e511e7544a497a44cfe94
ShannonZhong <zhongshiran@gmail.com>
2018-12-02 17:44:53 +0800

    Update 继承.js

--- 继承.js
+++ 继承.js
@@ -41,8 +41,58 @@ s.生命值 = ""
   Soldier.prototype.防御 = function() { /*要脸*/ }
   s = new Soldier()
 
-/*现在唯一的问题是 object s 还没有拥有parentCommom的 属性 */
+/*现在唯一的问题是 object s 还没有拥有parentCommom的 共有属性 */
 
+ function Human() {
+            this.name = "桃太狼"
+            this.肤色 = "你喜欢的颜色"
+        }
+ Human.prototype.eat = function() {}
+ Human.prototype.drink = function() {}
+ Human.prototype.poo = function() {}
+
+ function Soldier(name) {
+            this.ID = i
+            this.生命值 = 42
+            this.name = name || "无名战士"
+        }
+ Soldier.prototype.兵种 = "美国大兵"
+ Soldier.prototype.攻击力 = 5
+ Soldier.prototype.行走 = function() { /*走两步的代码*/ }
+ Soldier.prototype.奔跑 = function() { /*狂奔的代码*/ }
+ Soldier.prototype.死亡 = function() { /*passed away*/ }
+ Soldier.prototype.攻击 = function() { /*攻击的代码*/ }
+ Soldier.prototype.防御 = function() { /*要脸*/ }
+ Soldier.prototype.__proto__ = Human.prototype 
+
+ var s = new Soldier()
+/*现在唯一的问题是 object s 还没有拥有parentCommom的 私有属性 */
+/* Human.call(this) 这一句话里面的 this 是 function Soldier 里面的this相当于  把里面（function soldier ）的this 传到 外面（hunman prototype 里面去 ）
+
+function Human() {
+            this.name = "桃太狼"
+            this.肤色 = "你喜欢的颜色"
+        }
+Human.prototype.eat = function() {}
+Human.prototype.drink = function() {}
+Human.prototype.poo = function() {}
+
+function Soldier(name) {
+Human.call(this)
+this.ID = i
+this.生命值 = 42
+}
+        
+Soldier.prototype.兵种 = "美国大兵"
+Soldier.prototype.攻击力 = 5
+Soldier.prototype.行走 = function() { /*走两步的代码*/ }
+Soldier.prototype.奔跑 = function() { /*狂奔的代码*/ }
+Soldier.prototype.死亡 = function() { /*passed away*/ }
+Soldier.prototype.攻击 = function() { /*攻击的代码*/ }
+Soldier.prototype.防御 = function() { /*要脸*/ }
+Soldier.prototype.__proto__ = Human.prototype 
+
+ var s = new Soldier()        
 
 
 
