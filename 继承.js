d613b7ecb6dfab956385013d92bcbaaa90bbad4d
ShannonZhong <zhongshiran@gmail.com>
2018-12-02 16:53:45 +0800

    Update 继承.js

--- 继承.js
+++ 继承.js
@@ -1,1 +1,21 @@
+ var parentCommon = {
+            吃: '',
+            喝: '',
+            拉: '',
+            撒: ''
+        }
+        var soldierCommom = {
+            兵种: "美国大兵",
+            攻击力: 5,
+            行走: function() { /*走两步的代码*/ },
+            奔跑: function() { /*狂奔的代码*/ },
+            死亡: function() { /*passed away*/ },
+            攻击: function() { /*攻击的代码*/ },
+            防御: function() { /*要脸*/ }
 
+        }
+
+        soldierCommom.__proto__ = parentCommon
+        var s = {}
+        s.__proto__ = soldierCommom
+        /* 可以看出对象s 不仅拥有soldierCommom的所有 还拥有parentCommon*/
