bd40a555a9a996b13bb5bac294e81c8c58ea8b88
ShannonZhong <zhongshiran@gmail.com>
2019-02-11 21:45:53 +0800

    Update askYourSelf2.js

--- askYourSelf2.js
+++ askYourSelf2.js
@@ -1,4 +1,17 @@
-/* build your own curry function */
+/* build your own curry function
+作业是 柯里化 其他函数的 函数 
+
+*/
+
+
+
+
+
+
 /*guss the souce code:Array.prototype.splice/
 /*apply underscore.js to create a function*/
 /*anything left ？*/
+
+
+
+
